e2155b979dc03058323614bc9afed0631d7001a6
Giresse <mignongiresse@gmail.com>
2025-09-03 15:58:05 +0000

    MISE A JOUR DES FICHIERS

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('telephone');
+            $table->string('telephone')->nullable();
             $table->enum('role', ['client','prestataire','admin_part','admin']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
